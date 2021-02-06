6ebcb1728e3300243f972936e88d8f167a45a9be
Your Name <you@example.com>
2021-02-06 20:41:14 +0530

    code updated

---
+++ src/org/andrapradesh/NewClass.java
@@ -0,0 +1,13 @@
+package org.andrapradesh;
+
+public class NewClass {
+
+	private void compName() {
+		// TODO Auto-generated method stub
+
+	}
+	private void compNum() {
+		// TODO Auto-generated method stub
+
+	}
+}
